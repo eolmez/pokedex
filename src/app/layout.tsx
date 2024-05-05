928cbac812f8117ddcac16b210e49b2232820f33
eolmez <eolmez025@gmail.com>
2024-05-06 02:25:53 +0300

    favicon does not appear in vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
